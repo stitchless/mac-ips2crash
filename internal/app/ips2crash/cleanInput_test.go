9968899303e2b15951d5c5fa99ed0b44b907b690
Josh Peizer <contact@sent.sh>
2023-04-28 11:46:08 -0400

    Add test cases for cleaning input and output

--- internal/app/ips2crash/cleanInput_test.go
+++ internal/app/ips2crash/cleanInput_test.go
@@ -1,1 +1,104 @@
-package ips2crash
+package ips2crash_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/jpeizer/mac-ips2crash/internal/app/ips2crash"
+)
+
+func TestIsJSONObject(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    string
+		expected bool
+	}{
+		{
+			name:     "Valid JSON object",
+			input:    `{"key": "value"}`,
+			expected: true,
+		},
+		{
+			name:     "Valid JSON object with nested object",
+			input:    `{"key": {"key2": "value2"}}`,
+			expected: true,
+		},
+		{
+			name:     "Invalid JSON object",
+			input:    `{"key": "value",}`,
+			expected: false,
+		},
+		{
+			name:     "Empty string",
+			input:    ``,
+			expected: false,
+		},
+		{
+			name:     "JSON array",
+			input:    `["value1", "value2"]`,
+			expected: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := ips2crash.IsJSONObject(tc.input)
+			if result != tc.expected {
+				t.Errorf("IsJSONObject(%q) = %v; want %v", tc.input, result, tc.expected)
+			}
+		})
+	}
+}
+
+func TestNewCrashReport(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    string
+		expected ips2crash.InputFile
+	}{
+		{
+			name:  "Valid input file with all sections",
+			input: "Leading text\n{\"header\": \"value\"}\n{\n  \"payload\": {\n    \"nested\": \"data\"\n  }\n}\nTrailing text",
+			expected: ips2crash.InputFile{
+				LeadingText:  []string{"Leading text"},
+				Header:       "{\"header\": \"value\"}",
+				Payload:      "{  \"payload\": {    \"nested\": \"data\"  }}",
+				TrailingText: []string{"Trailing text"},
+			},
+		},
+		{
+			name:  "Input file with only header",
+			input: "{\"header\": \"value\"}",
+			expected: ips2crash.InputFile{
+				LeadingText:  nil,
+				Header:       "{\"header\": \"value\"}",
+				Payload:      "",
+				TrailingText: nil,
+			},
+		},
+		{
+			name:  "Empty input file",
+			input: "",
+			expected: ips2crash.InputFile{
+				LeadingText:  nil,
+				Header:       "",
+				Payload:      "",
+				TrailingText: nil,
+			},
+		},
+		// Add more test cases as needed
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := ips2crash.NewCrashReport([]byte(tc.input))
+			if err != nil {
+				t.Errorf("NewCrashReport(%q) returned unexpected error: %v", tc.input, err)
+			}
+
+			if !reflect.DeepEqual(result, tc.expected) {
+				t.Errorf("NewCrashReport(%q) = %+v; want %+v", tc.input, result, tc.expected)
+			}
+		})
+	}
+}
--- internal/app/ips2crash/cleanOutput_test.go
+++ internal/app/ips2crash/cleanOutput_test.go
@@ -1,1 +1,43 @@
 package ips2crash
+
+import "testing"
+
+func TestCleanOutput(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    IPSCrash
+		expected string
+	}{
+		{
+			name: "Valid input file with all sections",
+			input: IPSCrash{
+				LeadingText: []string{
+					"Leading text",
+				},
+				FormattedReport: "{\n  \"header\": \"value\"\n}",
+				TrailingText: []string{
+					"Trailing text",
+				},
+			},
+			expected: "Leading text\n{\n  \"header\": \"value\"\n}\nTrailing text\n",
+		},
+		{
+			name: "Input file with only header",
+			input: IPSCrash{
+				LeadingText:     nil,
+				FormattedReport: "{\n  \"header\": \"value\"\n}",
+				TrailingText:    nil,
+			},
+			expected: "{\n  \"header\": \"value\"\n}\n",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := CleanOutput(tc.input)
+			if result != tc.expected {
+				t.Errorf("CleanOutput(%v) = %+v; want %+v", tc.input, result, tc.expected)
+			}
+		})
+	}
+}
